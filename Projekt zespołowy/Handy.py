92f511375cd0c316fd4787a6ea316298f44494d7
Mateusz <252808@student.pwr.edu.pl>
2022-07-09 17:02:51 +0200

    DOdanie plików

--- Projekt zespołowy/Handy.py
+++ Projekt zespołowy/Handy.py
@@ -1,16 +1,17 @@
 import sys
 from PyQt5 import QtWidgets
-from window_app.main_window import Ui_MainWindow as win
+from window_app.main_window import Ui_main_window as win
+from PyQt5.QtGui import QIcon
 
 def end_application():
     ui.close_application()
 
 app = QtWidgets.QApplication(sys.argv)
+QtWidgets.QApplication.setWindowIcon(QIcon('logo.png'))
 app.setQuitOnLastWindowClosed(False)
 app.lastWindowClosed.connect(end_application)
 MainWindow = QtWidgets.QMainWindow()
 ui = win()
 ui.setupUi(MainWindow)
-MainWindow.showNormal()
-
+MainWindow.showMaximized()
 sys.exit(app.exec_())
--- Projekt zespołowy/controllers/camera_checker.py
+++ Projekt zespołowy/controllers/camera_checker.py
@@ -1,10 +1,13 @@
 import cv2
+
+
 class CameraChecker():
+
     def list_ports(self):
         index = 0
         arr = []
         while True:
-            cap = cv2.VideoCapture(index)
+            cap = cv2.VideoCapture(index, cv2.CAP_DSHOW)
             if not cap.read()[0]:
                 break
             else:
